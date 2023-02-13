e2e6124284de22f2c2f8b19ba0d9f46cbe2e3d90
Федоров Евгений <zen-g@mail.ru>
2023-02-13 11:13:33 +0300

    Edited Seminars
    Signed-off-by: Федоров Евгений <zen-g@mail.ru>

--- Seminar1/lecture1.java
+++ Seminar1/lecture1.java
@@ -1,8 +1,8 @@
 package Seminars.Seminar1;
 
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Random;
+//import java.time.LocalDateTime;
+//import java.util.Arrays;
+//import java.util.Random;
 
 public class lecture1 {
     public static void main(String[] args) {
